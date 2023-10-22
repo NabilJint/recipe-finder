fbf3dd1862785ee49b2d278886edcee7c3a5544a
Jecil <jecil.okeke@gmail.com>
2023-10-22 02:38:17 +0100

    Introduction of Models and enums

--- src/app/components/meal-planner/meal-planner.component.ts
+++ src/app/components/meal-planner/meal-planner.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FavoriteService } from 'src/app/services/favorite.service';
-import { RecipeService } from 'src/app/services/recipe.service';
+import { MealPlan, MealPlanDay, MealType, RecipeService, stringToMealPlanDay, stringToMealType } from 'src/app/services/recipe.service';
 
 @Component({
   selector: 'app-meal-planner',
@@ -9,14 +9,16 @@ import { RecipeService } from 'src/app/services/recipe.service';
 })
 export class MealPlannerComponent implements OnInit{
   selectedRecipe:any;
-  selectedDay!:any;
-  selectedMeal!:string;
+  mealPlanDayArray: string[] = Object.values(MealPlanDay);
+  mealPlanTypeArray: string[] = Object.values(MealType);
+  selectedDay!: string | null;
+  selectedMeal!: string | null;
   favorite:any[]=[]
-  mealPlans:any[]=[];
+  mealPlans:MealPlan[]=[];
 
 
 
- 
+
   ngOnInit(): void {
    this.getMealPlans();
    this.getFavorite();
@@ -28,27 +30,26 @@ export class MealPlannerComponent implements OnInit{
   getMealPlans(){
     this.mealPlans = this.recipeService.getMealPlan();
     console.log(this.mealPlans);
-    
+
   }
   addToMealPlans(recipe:any, day:string, meal:string){
-    this.recipeService.addToMealPlan(recipe,day,meal)
-    
+    this.recipeService.addToMealPlan(recipe,stringToMealPlanDay(day)!,stringToMealType(meal)!)
+
     this.getMealPlans()
   }
-  removeFromMealPlan(day:string, meal:string, recipe:any){
-    this.recipeService.removeFromMealPlan(day,meal,recipe);
+  removeFromMealPlan(recipe:any, day:string, meal:string){
+    this.recipeService.removeFromMealPlan(recipe,stringToMealPlanDay(day)!,stringToMealType(meal)!);
     this.getMealPlans()
   }
   clearSelection(){
     this.selectedRecipe = null;
-    
+
   }
   clearMealPlan() {
-    this.selectedDay = '';
-    this.selectedMeal = '';
-   
+    this.selectedDay = null;
+    this.selectedMeal = null;
   }
-  
+
   selectRecipe(recipe:any){
     this.selectedRecipe = recipe
   }
@@ -56,4 +57,19 @@ export class MealPlannerComponent implements OnInit{
  getFavorite(){
   this.favorite = this.favoriteRecipe.getFavoriteRecipes()
  }
+
+ get currentRecipes(): any[]{
+    var filter = this.mealPlans.filter((x) => x.Day == this.selectedDay);
+    var currentPlan = filter.length > 0? filter[0]: null;
+    if(currentPlan){
+      var filterTwo = currentPlan.Meals.filter((x) => x.Type == this.selectedMeal);
+      var currentMeal = filterTwo.length > 0? filterTwo[0]: null;
+      if(currentMeal){
+        return currentMeal.Recipes;
+      }
+      return [];
+    }
+    return [];
+ }
+
 }
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -3,6 +3,69 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
+export enum MealPlanDay {
+  Monday  = 'Monday',
+  Tuesday = 'Tuesday',
+  Wednesday = 'Wednesday',
+  Thursday = 'Thursday',
+  Friday = 'Friday',
+  Saturday = 'Saturday',
+  Sunday = 'Sunday'
+}
+
+export enum MealType {
+  Breakfast = 'Breakfast',
+  Lunch = 'Lunch',
+  Dinner = 'Dinner',
+  Snack = 'Snack'
+}
+
+export interface Meal {
+  Type: MealType
+  Recipes: any[];
+}
+export interface MealPlan {
+  Day: MealPlanDay;
+  Meals: Meal[];
+}
+
+export function stringToMealType(input: string): MealType | undefined {
+  // Convert the string to a MealType value
+  switch (input) {
+    case MealType.Breakfast:
+      return MealType.Breakfast;
+    case MealType.Lunch:
+      return MealType.Lunch;
+    case MealType.Dinner:
+      return MealType.Dinner;
+    case MealType.Snack:
+      return MealType.Snack;
+    default:
+      return undefined;
+  }
+}
+
+export function stringToMealPlanDay(input: string): MealPlanDay | undefined {
+  switch (input) {
+    case MealPlanDay.Monday:
+      return MealPlanDay.Monday;
+    case MealPlanDay.Tuesday:
+      return MealPlanDay.Tuesday;
+    case MealPlanDay.Wednesday:
+      return MealPlanDay.Wednesday;
+    case MealPlanDay.Thursday:
+      return MealPlanDay.Thursday;
+    case MealPlanDay.Friday:
+      return MealPlanDay.Friday;
+    case MealPlanDay.Saturday:
+      return MealPlanDay.Saturday;
+    case MealPlanDay.Sunday:
+      return MealPlanDay.Sunday;
+    default:
+      return undefined;
+  }
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,7 +74,7 @@ export class RecipeService {
   private apiUrl = 'https://api.spoonacular.com/recipes';
 
   favoriteRecipes: any[]=[];
-  maelplans:any={};
+  mealPlans: MealPlan[] = [];
 
   constructor(private http:HttpClient) { }
 
@@ -37,52 +100,110 @@ export class RecipeService {
     return this.http.get(url,{params})
   }
 
-  
-  
-  
+
+
+
 
   getMealPlan(){
-    return this.maelplans
+    this.sortMealPlan();
+    return this.mealPlans
   }
 
-  addToMealPlan(recipe:any, day:string, meal:string){
-    if(!this.maelplans[day]){
-      this.maelplans[day] ={};
-    }
-    if(!this.maelplans[day][meal]){
-      this.maelplans[day][meal] =[];
-    }
-    
-    this.maelplans[day][meal].push(recipe)
+  addToMealPlan(recipe:any, day:MealPlanDay, meal:MealType){
+    var dayExists: boolean = false;
+    for(let plan of this.mealPlans){
+      if(plan.Day == day){
+        var currentMeals = plan.Meals;
+        var mealExists = currentMeals.filter((x) => {
+          return x.Type == meal
+        }).length > 0? true : false;
 
-    // if(!day){
-    //   throw new Error('Day cannot be empty.');
-    //  }
-    //  if(!meal){
-    //   throw new Error('Meal cannot be empty')
-    //  }
-    //  const mealPlanIndex= this.maelplans.findIndex((mealPlan:any) => mealPlan.day === day && mealPlan.meal === meal)
-    //  if(mealPlanIndex === -1){
-    //  this.maelplans.push({
-    //   day,
-    //   meal,
-    //   recipes: [recipe]
-    //  })
-    //  } else{
-    //   this.maelplans[mealPlanIndex].recipes.push(recipe)
-    //  }
-    // }
-  
+          if(mealExists){
+            var indexMain: number;
+            for(var i = 0; i < plan.Meals.length; i++){
+              if(plan.Meals[i].Type == meal){
+                indexMain = i;
+              }
+            }
+            var filter = plan.Meals[indexMain!].Recipes.filter((x) => x.title == recipe.title);
+            var result = filter.length > 0? filter[0]: null;
+            if(!result){
+              plan.Meals[indexMain!].Recipes.push(recipe)
+            }
+          } else {
+            var newMeal: Meal = {
+              Type: meal,
+              Recipes: [recipe]
+            };
+            plan.Meals.push(newMeal);
+          }
+        dayExists = true;
+      }
+    }
+
+    if(!dayExists) {
+      var firstMeal: Meal = {
+        Type: meal,
+        Recipes: [recipe]
+      }
+      var newPlan: MealPlan = {
+        Day: day,
+        Meals: [firstMeal]
+      }
+
+      this.mealPlans.push(newPlan);
+    }
+
+    this.sortMealPlan();
   }
 
-  removeFromMealPlan(day:string, meal:string,recipe:any){
-    if(this.maelplans[day] && this.maelplans[day][meal]){
-      const index = this.maelplans[day][meal].findIndex((mealRecipe: { id: any; }) => mealRecipe.id === recipe.id)
-      if(index!== -1){
-        this.maelplans[day][meal].splice(index, 1);
+  sortByDay(a: MealPlan, b: MealPlan): number {
+    const daysOrder = Object.values(MealPlanDay);
+
+    const dayAIndex = daysOrder.indexOf(a.Day);
+    const dayBIndex = daysOrder.indexOf(b.Day);
+
+    return dayAIndex - dayBIndex;
+  }
+
+  sortMealPlan(){
+    this.mealPlans.sort(this.sortByDay);
+  }
+
+  removeFromMealPlan(recipe:any, day:MealPlanDay, meal:MealType){
+    for(let plan of this.mealPlans){
+      if(plan.Day == day){
+        for(let m of plan.Meals){
+          if(m.Type == meal){
+            var filter = m.Recipes.filter((x) => x.title == recipe.title);
+            var result = filter.length > 0? filter[0]: null;
+            if(result){
+              var index: number;
+              for(let i = 0; i < m.Recipes.length; i++){
+                if(m.Recipes[i].title == recipe.title){
+                  index = i;
+                }
+              }
+              m.Recipes.splice(index!, 1);
+            }
+            if(m.Recipes.length == 0){
+              var filterTwo = plan.Meals.filter((x) => x.Type == meal);
+              var resultTwo = filterTwo.length > 0? filterTwo[0]: null;
+              if(resultTwo){
+                var index: number;
+                for(let i = 0; i < plan.Meals.length; i++){
+                  if(plan.Meals[i].Type == meal){
+                    index = i;
+                  }
+                }
+                plan.Meals.splice(index!, 1);
+              }
+            }
+          }
+        }
       }
     }
   }
 
-  
+
 }
